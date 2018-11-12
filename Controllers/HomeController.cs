0da2e76740fbf973bc3f3a336e69741daf5dbf1c
Bernardo Mayol <bmayolv@hotmail.com>
2018-11-11 19:05:51 -0700

    Se hizo cambio en Home Controller action About

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace MvcApp.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application description page.MayolVerdugo";
 
             return View();
         }
